6c9841a45948d736c7da2728ac94127960f32349
Keraito <chakshunyu@gmail.com>
2021-10-04 14:11:13 +0200

    add test project

---
+++ deployment/native/styles.js
@@ -0,0 +1,7 @@
+import * as style0 from "Y:/Documents/Projects/widgets-resources/packages/pluggableWidgets/line-chart-web/tests/testProject/themesource/atlas_core/native/main";
+import * as style1 from "Y:/Documents/Projects/widgets-resources/packages/pluggableWidgets/line-chart-web/tests/testProject/themesource/myfirstmodule/native/main";
+import * as style2 from "Y:/Documents/Projects/widgets-resources/packages/pluggableWidgets/line-chart-web/tests/testProject/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2]);
