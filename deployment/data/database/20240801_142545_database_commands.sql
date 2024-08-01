de44a85ec1af5773d26e5cc0ade90b0e369fda29
leonardomendix <leonardo.de.souza@mendix.com>
2024-08-01 14:58:40 +0200

    Add first version of the project

---
+++ deployment/data/database/20240801_142545_database_commands.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "myfirstmodule$entity" ADD "attribute_4" VARCHAR_IGNORECASE(2147483647) NULL;
+INSERT INTO "mendixsystem$attribute" ("id", "entity_id", "attribute_name", "column_name", "type", "length", "default_value", "is_auto_number") VALUES ('e9f30548-ad49-4388-8816-6b6c124c3c0a', '89cbe983-34a3-4efd-b853-138670cdcd58', 'Attribute_4', 'attribute_4', 30, 0, '', false);
+UPDATE "mendixsystem$version" SET "versionnumber" = '4.2', "lastsyncdate" = '20240801 14:25:45';
