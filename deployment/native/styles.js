3b56c74f267cb0783420df80a3c49bde85092bfe
Illia Obukhau <8282906+iobuhov@users.noreply.github.com>
2025-06-12 11:57:42 +0200

    remove junk

--- deployment/native/styles.js
+++
@@ -1,9 +0,0 @@
-import * as style0 from "//Mac/Home/Documents/Projects/testProjects/themesource/nanoflowcommons/native/main";
-import * as style1 from "//Mac/Home/Documents/Projects/testProjects/themesource/webactions/native/main";
-import * as style2 from "//Mac/Home/Documents/Projects/testProjects/themesource/atlas_core/native/main";
-import * as style3 from "//Mac/Home/Documents/Projects/testProjects/themesource/myfirstmodule/native/main";
-import * as style4 from "//Mac/Home/Documents/Projects/testProjects/theme/native/main";
-
-import { flatten } from "mendix/native";
-
-module.exports = flatten([style0, style1, style2, style3, style4]);
